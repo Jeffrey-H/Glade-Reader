cb4cb0e97aa531654f1d4322be7e7e7bfa780576
jeffrey <jeffrey@qualide.nl>
2022-09-07 09:39:53 +0200

    first commit

---
+++ splitter.py
@@ -0,0 +1,19 @@
+import os
+from itertools import product
+
+from PIL import Image
+
+
+def tile(filename, dir_in, dir_out, d):
+    name, ext = os.path.splitext(filename)
+    img = Image.open(os.path.join(dir_in, filename))
+    w, h = img.size
+
+    grid = product(range(0, h - h % d, d), range(0, w - w % d, d))
+    for i, j in grid:
+        box = (j, i, j + d, i + d)
+        out = os.path.join(dir_out, f'{name}_{i}_{j}{ext}')
+        img.crop(box).save(out)
+
+
+tile("./image-resized.jpg", '.', './splitted', 66)
